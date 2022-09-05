a6dcc7761d3b95515e1000ef8e09982893e9e464
Александр Шешин <sheshin.alexanderkpc2052020@gmail.com>
2022-09-05 18:34:45 +0300

    a little comments

--- pages/main_page.py
+++ pages/main_page.py
@@ -17,25 +17,33 @@ class MainPage():
         self.browser.get(self.url)
         self.browser.find_element(*MainPageLocators.CROSS).click()
 
-    def tracks_to_file(self):
-        tracks_file = open("tracks_file.txt", "w+", encoding='utf-8')
+    def get_num_of_tracks(self):
         self.browser.execute_script(
             'window.scrollTo(0, document.body.scrollHeight);')
         sleep(1)
         num_of_tracks = self.browser.find_elements(
             By.CLASS_NAME, 'd-track')[-1].get_attribute("data-id")
         self.browser.execute_script('window.scrollTo(0, 0);')
-        for i in range(1, int(num_of_tracks)):
+        return int(num_of_tracks) + 1
+
+    def tracks_to_file(self):
+        tracks_file = open("tracks_file.txt", "w+", encoding='utf-8')
+        for i in range(1, self.get_num_of_tracks()):
             track = self.browser.find_element(
                 By.CSS_SELECTOR, f'[data-id="{i}"]')
             artists = track.find_elements(*MainPageLocators.ARTISTS)
+
             artists_names = ''
             for artist in artists:
                 artists_names += artist.get_attribute("title")
                 if artist.text != artists[-1].text:
-                    artists_names += '; '
+                    artists_names += '; '  # separator between artists
+
             track_name = track.find_element(*MainPageLocators.TRACK_NAME)
+
+            # line in file
             tracks_file.write(f'{artists_names} - {track_name.text}\n')
+
             self.browser.execute_script("arguments[0].scrollIntoView();",
                                         track)
         tracks_file.close()
